53f53de187812555b846c3fa1974fb1960d18b20
Anders Ekelund <anders.ekelund@grebban.com>
2025-06-04 16:54:32 +0200

    Initial commit

---
+++ test/grebban-aws-billing-notifications.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GrebbanAwsBillingNotifications from '../lib/grebban-aws-billing-notifications-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/grebban-aws-billing-notifications-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GrebbanAwsBillingNotifications.GrebbanAwsBillingNotificationsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
